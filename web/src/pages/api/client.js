e2c59e01375c9b397d0c46e3ed29719ece7d2a57
hakan.yavuz <hakan.yavuz@odeontechnology.com>
2023-05-16 21:22:28 +0300

    api, mongodb, axios dahil edildi.

--- web/src/pages/api/client.js
+++ web/src/pages/api/client.js
@@ -1,8 +1,7 @@
-
 import axios from 'axios';
 
 export function createClient() {
   return axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASEPATH,
+    baseURL: process.env.NEXT_PUBLIC_WEPPATH_API_URL,
   });
 }
