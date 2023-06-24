30f97a9cb0cf5b0317fbb746f64e5a0216859cb2
hakan.yavuz <hakan.yavuz@odeontechnology.com>
2023-06-24 15:35:47 +0300

    .

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.websocket$/,
+        use: "null-loader",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
