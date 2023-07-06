c9d525ac4f12fa030873b1a18b2119593f491e60
Melih Bektaş <melihbektas92@gmail.com>
2023-07-07 00:31:19 +0300

    Gallery Photo API Bağlandı Gallery Video API Bağlandı Local index kodları kaldırıldı

--- web/src/components/ImageGallery.js
+++ web/src/components/ImageGallery.js
@@ -1,37 +1,20 @@
 import React from 'react'
 import { Image } from 'antd';
 import { List } from 'antd';
-
-const ImageList = [
-  {ImageSrc:"images/Gallery-1.png"},
-  {ImageSrc:"images/Gallery-2.png"},
-  {ImageSrc:"images/Gallery-3.png"},
-  {ImageSrc:"images/Gallery-4.png"},
-  {ImageSrc:"images/Gallery-1.png"},
-  {ImageSrc:"images/Gallery-2.png"},
-  {ImageSrc:"images/Gallery-3.png"},
-  {ImageSrc:"images/Gallery-4.png"},
-  {ImageSrc:"images/Gallery-1.png"},
-  {ImageSrc:"images/Gallery-2.png"},
-  {ImageSrc:"images/Gallery-3.png"},
-  {ImageSrc:"images/Gallery-4.png"},
-  {ImageSrc:"images/Gallery-1.png"},
-  {ImageSrc:"images/Gallery-2.png"},
-
-  {ImageSrc:"images/Gallery-5.png"}
-
-]
+import { imageUrlBuilder } from "@/helpers/imageUrlBuilder";
 
 
-function ImageGallery() {
+function ImageGallery({data}) {
   return (
     <div> 
         <Image.PreviewGroup>
-          <List itemLayout="vertical" size="large" pagination={{ positin:"bottom", align: "center", pageSize: 12 }} grid={{ gutter: 18 ,column: 6 }}  dataSource={ImageList} renderItem={(item) => (
+          <List itemLayout="vertical" size="large" pagination={{ positin:"bottom", align: "center", pageSize: 12 }} grid={{ gutter: 18 ,column: 6 }}  dataSource={data} renderItem=
+          {(item) =>( 
             <List.Item>                
-                      <Image width={160} height={160} src={item.ImageSrc}/>            
+                      <Image width={160} height={160} src={imageUrlBuilder(item.ImageUrl)}  preview={{src: imageUrlBuilder(item.ImageUrl)}} />            
             </List.Item>           
-          )}/>          
+          )}/> 
+                   
         </Image.PreviewGroup>
     </div>
   )
--- web/src/pages/gallery/index.js
+++ web/src/pages/gallery/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Gallery from 'src/components/Gallery.js'
+import logger from "@/helpers/logger";
+import { createClient } from "@/pages/api/client";
 
-function index() {
+
+function index({data,data2}) {
   return (
     <div>
-      <Gallery/>
+      <Gallery data={data} data2={data2}/>
     </div>
   )
 }
@@ -12,3 +15,16 @@ function index() {
 export default index
 
 index.layout = "web";
+
+export const getServerSideProps = async () => {
+  try {
+    const client = createClient();
+    const res = await client.get("/gallery/photo");
+    const data = res.data;
+    const res2 = await client.get("/gallery/video");
+    const data2 = res2.data;
+    return { props: { data, data2 } };
+  } catch (error) {
+    logger.error(`AdminHome:${error}`);
+  }
+};
