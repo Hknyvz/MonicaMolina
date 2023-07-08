5bfc5f201bbc3e333fef9ff4124c28e893d4f41d
hakan.yavuz <hakan.yavuz@odeontechnology.com>
2023-07-08 19:31:41 +0300

    Logout buttonu eklendi

--- web/src/components/layouts/Navbar/NavbarMenu.js
+++ web/src/components/layouts/Navbar/NavbarMenu.js
@@ -1,31 +1,11 @@
-import { GlobalOutlined } from "@ant-design/icons";
-import { Avatar, Button, Dropdown, Popover, Space } from "antd";
-import Link from "next/link";
-import React, { useState } from "react";
+import { LogoutOutlined } from "@ant-design/icons";
+import { Button, Space } from "antd";
+import { signOut, useSession } from "next-auth/react";
+import React from "react";
 
 const NavbarMenu = React.memo(function NavBarMenu() {
-  const [state, setState] = useState({
-    flag: "en",
-  });
-
-  const onFlagChangeHandle = (value, e) => {
-    e.preventDefault();
-    setState({
-      ...state,
-      flag: value,
-    });
-  };
-
-  const items = [
-    {
-      key: 1,
-      label: (
-        <Link onClick={(e) => onFlagChangeHandle("en", e)} href="#">
-          <span>English</span>
-        </Link>
-      ),
-    },
-  ];
+  const { status } = useSession();
+  console.log(status);
 
   return (
     <div
@@ -42,30 +22,16 @@ const NavbarMenu = React.memo(function NavBarMenu() {
     >
       <Space>
         <div>
-          <Dropdown placement="bottomRight" menu={{ items }}>
-            <Link href="#">
-              <GlobalOutlined />
-            </Link>
-          </Dropdown>
-        </div>
-        <div>
-          {/* {user && (
-            <Popover placement="bottomRight" content={userContent}>
-              <Link href="#">
-                <Avatar src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png" />
-                <span>{user?.fullName}</span>
-              </Link>
-            </Popover>
-          )}
-          {!user && (
+          {status ? (
             <Button
-              onClick={() => {
-                window.location.href = "/auth/login";
-              }}
+              type="primary"
+              style={{ background: "red" }}
+              onClick={() => signOut()}
             >
-              Login
+              <LogoutOutlined />
+              Logout
             </Button>
-          )} */}
+          ) : null}
         </div>
       </Space>
     </div>
