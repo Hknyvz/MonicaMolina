201c5628f8feecd2e429e47defa73bc388e45065
hakan.yavuz <hakan.yavuz@odeontechnology.com>
2023-07-21 01:21:47 +0300

    Carousel tablosu tamamlandı.

--- web/src/components/admin/home/carousel/CarouselTable.js
+++ web/src/components/admin/home/carousel/CarouselTable.js
@@ -4,7 +4,6 @@ import CarouselCreateModal from "./CarouselCreateModal";
 import { createClient } from "@/pages/api/client";
 import {
   FullSpace,
-  TableCell,
   TableContainer,
   TableGeneralOperationContainer,
 } from "@/components/admin/shared/StyledComponent";
@@ -18,16 +17,29 @@ import {
 } from "@ant-design/icons";
 import TableRowAction from "../../shared/TableRowAction";
 import CarouselDetailEditModal from "./CarouselDetailUpdateModal";
+import Link from "next/link";
+import LinkUpdateModal from "@/components/admin/home/carousel/LinkUpdateModal";
+import { SiItunes } from "react-icons/si";
+import { SlSocialSpotify, SlSocialYoutube } from "react-icons/sl";
 
 function CarouselTable({ data }) {
   const [isOpenCreateModal, setIsOpenCreateModal] = useState(false);
   const [isOpenUpdateModal, setIsOpenUpdateModal] = useState(false);
   const [isOpenUpdateDetailModal, setIsOpenUpdateDetailModal] = useState(false);
+  const [isOpenUpdateYoutubeModal, setIsOpenUpdateYoutubeModal] =
+    useState(false);
+  const [isOpenUpdateSpotifyModal, setIsOpenUpdateSpotifyModal] =
+    useState(false);
+  const [isOpenUpdateITunesModal, setIsOpenUpdateITunesModal] = useState(false);
+  const [iconUpdateLinkModal, setIconUpdateLinkModal] = useState();
+  const [titleUpdateLinkModal, setTitleUpdateLinkModal] = useState();
+  const [linkUpdateLinkModal, setLinkUpdateLinkModal] = useState();
   const [updateData, setUpdateData] = useState();
   const [tableData, setTableData] = useState(data);
   const client = createClient();
-  const apiUrl = "/home/carousel";
+  const apiUrl = "/home/carousel?admin=true";
   const detailApiUrl = "/home/carousel-detail";
+  const detailStatusApiUrl = "/home/carousel-status";
   const notification = useContext(NotificationContext);
 
   const handleDelete = async (id) => {
@@ -36,14 +48,29 @@ function CarouselTable({ data }) {
     await refreshData();
   };
 
-  const handleStatusChange = (checked, item) => {
+  const handleStatusChange = async (checked, item) => {
     if (!item.DetailImageUrl || !item.DetailTitle) {
+      notification.error({
+        message:
+          "Please enter the detail image and detail title. Without these, you cannot update the status.",
+      });
       return;
     }
-    const updatedTableData = tableData.map((dataItem) =>
-      dataItem === item ? { ...dataItem, HaveDetail: checked } : dataItem
-    );
-    setTableData(updatedTableData);
+    try {
+      const res = await client.put(detailStatusApiUrl, {
+        _id: item._id,
+        HaveDetail: checked,
+      });
+      if (res.status === 200) {
+        notification.success({ message: "Successful" });
+        const updatedTableData = tableData.map((dataItem) =>
+          dataItem === item ? { ...dataItem, HaveDetail: checked } : dataItem
+        );
+        setTableData(updatedTableData);
+      }
+    } catch (error) {
+      notification.error({ message: error.toString() });
+    }
   };
 
   const columns = [
@@ -118,6 +145,63 @@ function CarouselTable({ data }) {
               ></Image>
             ),
         },
+        {
+          title: "Links",
+          render: (data) => (
+            <>
+              <FullSpace style={{ justifyContent: "center" }} size={(0, 20)}>
+                {data.YoutubeLink && (
+                  <Link
+                    href=""
+                    onClick={() => {
+                      setIsOpenUpdateYoutubeModal(true);
+                      setTitleUpdateLinkModal("Youtube Link");
+                      setIconUpdateLinkModal(<SlSocialYoutube />);
+                      setLinkUpdateLinkModal({
+                        _id: data._id,
+                        Link: data.YoutubeLink,
+                      });
+                    }}
+                  >
+                    <img src="/youtube.svg" width={75} />
+                  </Link>
+                )}
+                {data.ItunesLink && (
+                  <Link
+                    href=""
+                    onClick={() => {
+                      setIsOpenUpdateITunesModal(true);
+                      setTitleUpdateLinkModal("Itunes Link");
+                      setIconUpdateLinkModal(<SiItunes />);
+                      setLinkUpdateLinkModal({
+                        _id: data._id,
+                        Link: data.ItunesLink,
+                      });
+                    }}
+                  >
+                    <img src="/apple_music.svg" width={75} />
+                  </Link>
+                )}
+                {data.SpotifyLink && (
+                  <Link
+                    href=""
+                    onClick={() => {
+                      setIsOpenUpdateSpotifyModal(true);
+                      setTitleUpdateLinkModal("Spotify Link");
+                      setIconUpdateLinkModal(<SlSocialSpotify />);
+                      setLinkUpdateLinkModal({
+                        _id: data._id,
+                        Link: data.SpotifyLink,
+                      });
+                    }}
+                  >
+                    <img src="/spotify.svg" width={75} />
+                  </Link>
+                )}
+              </FullSpace>
+            </>
+          ),
+        },
       ],
     },
     {
@@ -175,6 +259,13 @@ function CarouselTable({ data }) {
   const refreshData = async () => {
     const res = await client.get(apiUrl);
     setTableData(res.data);
+  };
+
+  const resetLinkModal = (setVisible) => {
+    setVisible(false);
+    setIconUpdateLinkModal("");
+    setLinkUpdateLinkModal("");
+    setTitleUpdateLinkModal("");
   };
 
   return (
@@ -269,6 +360,78 @@ function CarouselTable({ data }) {
           }}
         />
       )}
+      <LinkUpdateModal
+        onOk={async (data) => {
+          try {
+            const res = await client.put(detailApiUrl, {
+              _id: data._id,
+              YoutubeLink: data.Link,
+            });
+            if (res.status === 200) {
+              await refreshData();
+              resetLinkModal(setIsOpenUpdateYoutubeModal);
+              notification.success({ message: "Successful" });
+            }
+          } catch (error) {
+            notification.error({ message: error.toString() });
+          }
+        }}
+        onCancel={() => {
+          resetLinkModal(setIsOpenUpdateYoutubeModal);
+        }}
+        record={linkUpdateLinkModal}
+        icon={iconUpdateLinkModal}
+        title={titleUpdateLinkModal}
+        visible={isOpenUpdateYoutubeModal}
+      />
+      <LinkUpdateModal
+        onOk={async (data) => {
+          try {
+            const res = await client.put(detailApiUrl, {
+              _id: data._id,
+              SpotifyLink: data.Link,
+            });
+            if (res.status === 200) {
+              await refreshData();
+              resetLinkModal(setIsOpenUpdateSpotifyModal);
+              notification.success({ message: "Successful" });
+            }
+          } catch (error) {
+            notification.error({ message: error.toString() });
+          }
+        }}
+        onCancel={() => {
+          resetLinkModal(setIsOpenUpdateSpotifyModal);
+        }}
+        record={linkUpdateLinkModal}
+        icon={iconUpdateLinkModal}
+        title={titleUpdateLinkModal}
+        visible={isOpenUpdateSpotifyModal}
+      />
+      <LinkUpdateModal
+        onOk={async (data) => {
+          try {
+            const res = await client.put(detailApiUrl, {
+              _id: data._id,
+              ItunesLink: data.Link,
+            });
+            if (res.status === 200) {
+              await refreshData();
+              resetLinkModal(setIsOpenUpdateITunesModal);
+              notification.success({ message: "Successful" });
+            }
+          } catch (error) {
+            notification.error({ message: error.toString() });
+          }
+        }}
+        onCancel={() => {
+          resetLinkModal(setIsOpenUpdateITunesModal);
+        }}
+        record={linkUpdateLinkModal}
+        icon={iconUpdateLinkModal}
+        title={titleUpdateLinkModal}
+        visible={isOpenUpdateITunesModal}
+      />
     </>
   );
 }
--- web/src/pages/api/home/carousel-detail.js
+++ web/src/pages/api/home/carousel-detail.js
@@ -15,7 +15,21 @@ const handler = async (req, res) => {
   await dbConnect("Monica");
   const method = req.method;
   try {
-    if (method === "PUT") {
+    if (method === "GET") {
+      const { id } = req.query;
+      let carousels = await CarouselModel.find(
+        { _id: id },
+        {
+          Order: 0,
+          ImageUrl: 0,
+          HaveDetail: 0,
+          createdAt: 0,
+          updatedAt: 0,
+          __v: 0,
+        }
+      ).exec();
+      return res.status(200).json(carousels.sort((a, b) => a.Order - b.Order));
+    } else if (method === "PUT") {
       const response = await update(req.body);
       return res.status(200).json(response);
     } else {
@@ -23,17 +37,17 @@ const handler = async (req, res) => {
     }
   } catch (err) {
     logger.error(`Api Carousel:${err}`);
-    return res.status(500).json({ message: "Couldn't save file", err: err });
+    return res.status(500).json({ message: "Couldn't save", err: err });
   }
 };
 const directory = "home-detail-photo";
 const update = async (data) => {
-  const model = {
-    DetailTitle: data.DetailTitle,
-    SPotifyLink: data.SpotifyLink,
-    YoutubeLink: data.YoutubeLink,
-    ItunesLink: data.ITunesLink,
-  };
+  const model = {};
+  if (data.DetailTitle) model.DetailTitle = data.DetailTitle;
+  if (data.SpotifyLink) model.SpotifyLink = data.SpotifyLink;
+  if (data.YoutubeLink) model.YoutubeLink = data.YoutubeLink;
+  if (data.ItunesLink) model.ItunesLink = data.ItunesLink;
+
   if (data.DetailImageUrl) {
     deleteImage(data._id, directory);
     model.DetailImageUrl = await saveImage(
@@ -42,11 +56,12 @@ const update = async (data) => {
       directory
     );
   }
+  console.log(data._id, model);
 
-  console.log(model, data._id);
   let updatedEntry = await CarouselModel.findByIdAndUpdate(data._id, model, {
     new: true,
   });
+  console.log(updatedEntry);
   const res = { message: "Updated successful", data: updatedEntry };
   return res;
 };
