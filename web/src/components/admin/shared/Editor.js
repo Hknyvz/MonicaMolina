392cc67d64a3330903f1e861dfc219e740726618
hakan.yavuz <hakan.yavuz@odeontechnology.com>
2024-04-02 03:03:48 +0300

    fix

--- web/src/components/admin/shared/Editor.js
+++ web/src/components/admin/shared/Editor.js
@@ -1,31 +1,32 @@
 import React from "react";
 import QuillToolbar, { formats, modules } from "./EditorToolbar";
 import dynamic from "next/dynamic";
+import RichTextEditor from "./EditorTest";
 
-const Quill = dynamic(
-  () => {
-    return import("react-quill");
-  },
-  { ssr: false }
-);
+// const Quill = dynamic(() => import("react-quill"), { ssr: false });
 
 const Editor = ({ onChange, value }) => {
   return (
-    <div>
-      <QuillToolbar />
-      <div className="quillEditor">
-        {document && (
-          <Quill
-            theme="snow"
-            value={value}
-            onChange={onChange}
-            placeholder={"Write something awesome..."}
-            modules={modules}
-            formats={formats}
-          />
-        )}
-      </div>
-    </div>
+    // <div>
+    //   <QuillToolbar />
+    //   <div className="quillEditor">
+    //     {document && (
+    //       <Quill
+    //         theme="snow"
+    //         value={value}
+    //         onChange={onChange}
+    //         placeholder={"Write something awesome..."}
+    //         modules={modules}
+    //         formats={formats}
+    //       />
+    //     )}
+    //   </div>
+    // </div>
+    <RichTextEditor
+      placeholder={"Write something awesome..."}
+      setValue={onChange}
+      value={value}
+    />
   );
 };
 
