a8d1a35b0f17428d5c107b26651606a289475554
hakan.yavuz <hakan.yavuz@odeontechnology.com>
2024-04-03 22:50:33 +0300

    fix

--- web/src/pages/contact/index.js
+++ web/src/pages/contact/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import Contact from 'src/components/Contact.js'
+import React from "react";
+import Contact from "src/components/Contact.js";
 
 function index() {
   return (
     <div>
-      <Contact/>
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
 
 index.layout = "web";
+
+export const getServerSideProps = async () => {
+  try {
+    const client = createClient();
+    const res = await client.get("/home/carousel");
+    const data = res.data;
+    return { props: { data } };
+  } catch (error) {
+    logger.error(`AdminHome:${error}`);
+  }
+};
